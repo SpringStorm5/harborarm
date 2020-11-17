3202f659dd0b856c24980aae2eff2356c3b2f762
Galina Nikitina <galya@linnovate.net>
2020-11-17 11:39:33 +0300

    change added sleep

--- scripts/harbor.sh
+++ scripts/harbor.sh
@@ -66,15 +66,15 @@ echo "Docker Compose Installation done"
 
 sleep 60
 #Install Latest Stable Harbor Release
-cd /var/www/
+#cd /var/www/
 # HARBORVERSION=$(curl -s https://github.com/goharbor/harbor/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
 # curl -s https://api.github.com/repos/goharbor/harbor/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
-cp /var/lib/waagent/custom-script/download/1/harbor-online-installer-v2.1.1.tgz .
-tar xvf harbor-online-installer-v2.1.1
+#cp /var/lib/waagent/custom-script/download/1/harbor-online-installer-v2.1.1.tgz .
+#tar xvf harbor-online-installer-v2.1.1
 #tar xvf harbor-online-installer-v$HARBORVERSION.tgz
-cd harbor
+#cd harbor
 # Create Self-Signed OpenSSL Certs
-cd /var/www/harbor/
+#cd /var/www/harbor/
 
 mkdir -p /var/www/harbor/data/secret/cert
 cd /var/www/harbor/data/secret/cert
@@ -147,6 +147,7 @@ echo "Clean up the input dir"
 rm -rf ${harbor_prepare_path}/input
 EOF
 chmod +x ./prepare
+cat ./prepare | grep cert_dir
 
 cat <<\EOF >> harbor.yml
 # Configuration file of Harbor
@@ -324,6 +325,8 @@ EOF
 #sed -e '/\/your\/private\/key\/path$/ s/^#*/#/' -i harbor.yml
 # PASS=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 # sed -i "s/.*harbor_admin_password: Harbor12345*/harbor_admin_password: $PASS/" harbor.yml
+
+cd /var/www/harbor/
 ./install.sh --with-clair --with-chartmuseum
 docker ps
 echo -e "Harbor Installation Complete \n\nPlease log out and log in or run the command 'newgrp docker' to use Docker without sudo\n\nLogin to your harbor instance:\n docker login -u admin -p Harbor12345 $IPorFQDN"
